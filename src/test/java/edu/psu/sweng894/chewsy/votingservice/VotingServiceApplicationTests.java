739104432fc5a227f7815a1131d1542d001cd3cf
tim <timmc9104@gmail.com>
2022-07-06 19:37:06 -0400

    AB#66 - Initialize source code

---
+++ src/test/java/edu/psu/sweng894/chewsy/votingservice/VotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psu.sweng894.chewsy.votingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
